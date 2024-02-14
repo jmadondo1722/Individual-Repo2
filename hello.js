4280861bdd571630319b452dfac2bfd001ac750f
jmadondo1722 <jmadondo1722@gmail.com>
2024-02-14 19:44:23 +0200

    Added hello.js with getHello function

---
+++ hello.js
@@ -0,0 +1,10 @@
+// hello.js
+
+// Function that returns the word hello
+function getHello() {
+    return "hello";
+  }
+  
+  // Call the function and print "Hello" with the name
+  console.log("Word", getHello());
+  
